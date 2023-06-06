69f1e0709f73995bb71a2218a151650f1c2f1484
Zak Mayfield <zakmayfield@gmail.com>
2023-06-06 09:36:30 -0400

    adjust hero styles

--- src/features/home/components/Hero.tsx
+++ src/features/home/components/Hero.tsx
@@ -4,7 +4,7 @@ interface HeroProps {}
 
 export const Hero: FC<HeroProps> = ({}) => {
   return (
-    <div className='px-6 min-h-[500px] flex items-center border'>
+    <div className='px-6 h-[75vh] md:h-[50vh] flex items-center'>
       <HeroContent />
     </div>
   );
@@ -13,9 +13,9 @@ export const Hero: FC<HeroProps> = ({}) => {
 function HeroContent() {
   return (
     <div className='w-full'>
-      <div className='flex flex-col items-end md:items-start justify-center border '>
-        <h1 className='text-3xl md:text-7xl tracking-wide'>Zachary Mayfield</h1>
-        <h2 className='text-xl md:text-2xl leading-loose'>
+      <div className='flex flex-col items-end md:items-start justify-center '>
+        <h1 className='text-4xl md:text-7xl tracking-wide'>Zachary Mayfield</h1>
+        <h2 className='text-2xl md:text-2xl leading-loose'>
           Front end developer
         </h2>
       </div>
