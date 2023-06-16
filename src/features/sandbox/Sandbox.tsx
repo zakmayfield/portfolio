37f6fbc156dc6f07b11604905437f8764a8d564b
Zak Mayfield <zakmayfield@gmail.com>
2023-06-16 13:02:15 -0400

    rendering a sign in button if no session

--- src/features/sandbox/Sandbox.tsx
+++ src/features/sandbox/Sandbox.tsx
@@ -1,13 +1,16 @@
-import { FC } from 'react';
+import { getAuthSession } from '@/lib/auth';
 import { ContentContainer } from '@/shared/components';
+import Link from 'next/link';
 
-interface SandboxProps {}
-
-export const Sandbox: FC<SandboxProps> = ({}) => {
+export const Sandbox = async ({}) => {
+  const session = await getAuthSession();
   return (
     <div>
       <ContentContainer className='md:max-w-2xl mx-auto p-6 bg-slate-200'>
         Sandbox
+        <div className='text-center'>
+          {session ? 'logged in' : <Link href='/sign-up'>Sign Up</Link>}
+        </div>
       </ContentContainer>
     </div>
   );
