0d073e36b8db46096a893b3d66fb58427339fd95
Zak Mayfield <zakmayfield@gmail.com>
2023-06-19 09:58:32 -0400

    showing json

--- src/features/sandbox/Sandbox.tsx
+++ src/features/sandbox/Sandbox.tsx
@@ -11,6 +11,9 @@ export const Sandbox: FC<SandboxProps> = ({ session }) => {
     <div className='flex items-end h-full gap-6'>
       <div className='border-4 self-start border-slate-200 p-6 bg-slate-100 w-2/3 h-full rounded-md'>
         Welcome to the sandbox dashboard.
+        <div>
+          <pre className='text-xs'>{JSON.stringify(session, null, 2)}</pre>
+        </div>
       </div>
       <div className='border-4 border-slate-200 bg-slate-100 p-6 flex-grow h-full rounded-md'>
         {session?.user ? <Authn /> : <Unauthn />}
