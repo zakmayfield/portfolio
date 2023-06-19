06c85b951ba9aa9da7221e5c79032ccd19c4865b
Zak Mayfield <zakmayfield@gmail.com>
2023-06-19 14:46:30 -0400

    updated project page to be much better looking

--- src/features/project/Project.tsx
+++ src/features/project/Project.tsx
@@ -3,6 +3,7 @@ import { Button, ContentContainer, FullHeightImage } from '@/shared/components';
 import { ProjectType } from '@/shared/types';
 import { FC } from 'react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 interface ProjectProps {
   project: ProjectType;
@@ -13,27 +14,25 @@ export const Project: FC<ProjectProps> = ({ project }) => {
   return (
     <div className='p-6'>
       <ContentContainer className='max-w-4xl flex flex-col gap-6'>
-        <FullHeightImage
-          image={project.image}
-          altText='Chasers Fresh Juice landing page'
-        />
-        {/* <div className='flex flex-col md:flex-row gap-6 border p-6'>
-          <div className='flex mx-auto border-4'>
-            <div className='md:w-2/3 overflow-hidden rounded-md shadow-lg'>
-             
-            </div>
-            <div className='p-6 border'>
-              <h3 className='text-xl font-semibold'>Stack</h3>
-              <ul className='mt-4 flex flex-wrap md:flex-col gap-6 md:gap-2'>
-                <li className='p-2 border-2 rounded-md'>TypeScript</li>
-                <li className='p-2 border-2 rounded-md'>Next.js v13+</li>
-                <li className='p-2 border-2 rounded-md'>Chakra UI</li>
-                <li className='p-2 border-2 rounded-md'>Framer Motion</li>
-                <li className='p-2 border-2 rounded-md'>Node.js</li>
-              </ul>
-            </div>
+        <div className='flex flex-col md:flex-row gap-6 justify-center'>
+          <FullHeightImage
+            image={project.image}
+            altText={`${project.name} landing page`}
+            className='w-full md:w-2/3'
+            rounded
+          />
+          <div className='p-6'>
+            <h3 className='text-xl font-semibold'>Stack</h3>
+            <ul className='mt-4 flex flex-wrap md:flex-col gap-6 md:gap-2'>
+              <li className='p-2 border-2 rounded-md'>TypeScript</li>
+              <li className='p-2 border-2 rounded-md'>Next.js v13+</li>
+              <li className='p-2 border-2 rounded-md'>Chakra UI</li>
+              <li className='p-2 border-2 rounded-md'>Framer Motion</li>
+              <li className='p-2 border-2 rounded-md'>Node.js</li>
+            </ul>
           </div>
-        </div> */}
+        </div>
+
         <div className='p-6 indent-2 tracking-wide leading-relaxed text-lg max-w-[75ch] mx-auto'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, nobis
           et necessitatibus odio delectus fugiat, qui reprehenderit eius est
--- src/shared/components/FullHeightImage.tsx
+++ src/shared/components/FullHeightImage.tsx
@@ -7,16 +7,22 @@ interface FullHeightImageProps {
   image: StaticImageData;
   altText: string;
   className?: string;
+  rounded?: boolean;
 }
 
 export const FullHeightImage: FC<FullHeightImageProps> = ({
   image,
   altText,
   className,
+  rounded = false,
 }) => {
   return (
     <div className={cn('', className)}>
-      <Image src={image} alt={altText} className='object-cover w-full' />
+      <Image
+        src={image}
+        alt={altText}
+        className={`object-cover h-full ${rounded ? 'rounded-md' : ''}`}
+      />
     </div>
   );
 };
