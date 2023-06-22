11fc3a520370032e65974f336bf4f89b7cae8724
Zak Mayfield <zakmayfield@gmail.com>
2023-06-22 15:11:51 -0400

    contact form and hook

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,9 @@
+import { Contact } from '@/features/contact';
+
 export const metadata = {
   title: 'Contact | Zachary Mayfield',
 };
 
 export default function ContactPage() {
-  return (
-    <main>
-      <div className='text-center'>
-        <h1 className='font-extralight text-3xl'>Contact Page</h1>
-      </div>
-    </main>
-  );
+  return <Contact />;
 }
