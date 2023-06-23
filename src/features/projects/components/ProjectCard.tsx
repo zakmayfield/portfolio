dd097f7161f59cf01c50564580a2279970d4a0cd
Zak Mayfield <zakmayfield@gmail.com>
2023-06-23 11:27:35 -0400

    starting to adjust project cards for mobile

--- src/features/projects/components/ProjectCard.tsx
+++ src/features/projects/components/ProjectCard.tsx
@@ -10,7 +10,7 @@ interface ProjectCardProps {
 export const ProjectCard: FC<ProjectCardProps> = ({ project }) => {
   return (
     <FadeIn>
-      <ContentContainer className='max-w-3xl flex justify-between items-center border rounded-md p-3 shadow-md'>
+      <ContentContainer className='max-w-md md:max-w-3xl flex flex-col-reverse md:flex-row justify-between md:items-center border rounded-md p-3 shadow-md'>
         <div>
           <h3 className='text-lg mb-3'>{project.name}</h3>
           <ul className='flex gap-6 text-sm'>
@@ -23,7 +23,6 @@ export const ProjectCard: FC<ProjectCardProps> = ({ project }) => {
           </div>
         </div>
 
-        {/* TODO: add hover that shows overlay with 'View' as text */}
         <Link href={`/projects/${project.slug}`} className='w-48'>
           <FullHeightImage
             image={project.image}
@@ -32,6 +31,27 @@ export const ProjectCard: FC<ProjectCardProps> = ({ project }) => {
           />
         </Link>
       </ContentContainer>
+      {/* <ContentContainer className='max-w-3xl flex justify-between items-center border rounded-md p-3 shadow-md'>
+        <div>
+          <h3 className='text-lg mb-3'>{project.name}</h3>
+          <ul className='flex gap-6 text-sm'>
+            {project.stack.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+          <div className='w-[35ch] whitespace-nowrap overflow-hidden text-ellipsis mt-6'>
+            {project.description}
+          </div>
+        </div>
+
+        <Link href={`/projects/${project.slug}`} className='w-48'>
+          <FullHeightImage
+            image={project.image}
+            altText={`${project.name} landing page`}
+            rounded
+          />
+        </Link>
+      </ContentContainer> */}
     </FadeIn>
   );
 };
