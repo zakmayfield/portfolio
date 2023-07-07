a5e43e050b206731b32fce9911b039f5dc6a028e
Zak Mayfield <zakmayfield@gmail.com>
2023-07-07 08:47:45 -0400

    adjusted user image and some styles for dashboard

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -4,6 +4,8 @@ import { ContentContainer } from '@/shared/components';
 import { useSessionContext } from '@/shared/context/SessionContext';
 import { FC } from 'react';
 import { unbounded } from '@/utils/fonts';
+import { Edit } from 'lucide-react';
+import Image from 'next/image';
 
 interface DashboardProps {}
 
@@ -16,22 +18,44 @@ export const Dashboard: FC<DashboardProps> = ({}) => {
       </h1>
 
       <div className='flex items-center gap-12 w-full justify-between px-6'>
-        <div className='p-12'>img</div>
+        {/* https://robohash.org/52 */}
+        <div className='p-12'>
+          {session?.user.image ? (
+            <Image
+              src={session.user.image}
+              alt='profile picture'
+              width={100}
+              height={100}
+              className='rounded-full'
+            />
+          ) : (
+            <Image
+              src={`https://robohash.org/52?set=set2&size=300x300`}
+              alt='image of robot'
+              width={300}
+              height={300}
+              className='rounded-full mx-auto'
+            />
+          )}
+        </div>
 
-        <div className='flex-1 flex flex-col gap-6'>
-          <div>
-            <h3 className='font-thin'>Name</h3>
-            <p>{session?.user.name}</p>
+        <div className='flex-1 flex flex-col gap-6 bg-zinc-50 px-6 py-12 rounded-md'>
+          <div className='border-b pb-6'>
+            <h3 className='font-thin mb-2'>Name</h3>
+            <p className='text-lg'>{session?.user.name}</p>
+          </div>
+
+          <div className='border-b pb-6'>
+            <h3 className='font-thin mb-2'>Email</h3>
+            <p className='text-lg'>{session?.user.email}</p>
           </div>
 
           <div>
-            <h3 className='font-thin'>Email</h3>
-            <p>{session?.user.email}</p>
-          </div>
-
-          <div>
-            <h3 className='font-thin'>Username</h3>
-            <p>{session?.user.username}</p>
+            <h3 className='font-thin mb-2'>Username</h3>
+            <div className='flex items-center gap-6'>
+              <p className='text-lg'>{session?.user.username}</p>
+              <Edit size={20} className='cursor-pointer' />
+            </div>
           </div>
         </div>
       </div>
