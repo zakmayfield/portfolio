347ce4ecc1f40746d8eae4b6cb6c06a8a2f8aca6
Zak Mayfield <zakmayfield@gmail.com>
2023-07-18 10:17:14 -0400

    added challenges to project page

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -52,9 +52,11 @@ export const projects: ProjectsType = [
       'Framer Motion',
       'Node.js',
     ],
+    url: 'https://www.chasersjuice.com',
     description:
       "I was a front end developer apart of a small team team to rebuild Chasers Fresh Juice's website. The Chasers Fresh Juice website was built with the latest version of Next.js, utilizing many amazing features such as image optimization, server side rendering, and client-side routing for seamless navigation. Our main objective was to enhace the user interface and provide users with a clean and modern UI. I was also tasked with building an Express webhook for the websites contact form. The stack for this project is TypeScript, Next.js, Chakra UI, Framer Motion and Node.js.",
-    url: 'https://www.chasersjuice.com',
+    challenges:
+      "One of the notable challenges I faced during this project was the implementation of an Express webhook for the website's contact form. This involved integrating Node.js to ensure reliable communication between the website and the server. This challenge required careful attention to detail and an understanding of backend technologies.",
   },
   {
     id: 2,
@@ -62,9 +64,11 @@ export const projects: ProjectsType = [
     image: DominosHome,
     slug: 'dominos',
     stack: ['JavaScript', 'React', 'Storybook'],
+    url: 'https://www.dominos.com',
     description:
       "During my time at Dominos, I played a crucial role as a front end React developer, actively contributing to the continuous development and enhancement of the Dominos web app. My primary focus was on ensuring the smooth operation and improvement of user interfaces. Leveraging my skills in React and JavaScript, I actively participated in maintaining and elevating the app's functionality. I collaborated with Dominos UX/UI designers to ensure consistensy across the front end and delivered high quality, trusted code. The stack I utilized here was React, JavaScript, and Storybook",
-    url: 'https://www.dominos.com',
+    challenges:
+      'I would overcome several challenges in my postion here at Dominos, such as ensuring accesibility and cross browser compatibility, as well as scalability for a large production environment, and ensuring code was performant and clean. I learned how to build UI components in Storybook to ensure reuseability and scalability for the front end.',
   },
   {
     id: 3,
@@ -72,8 +76,10 @@ export const projects: ProjectsType = [
     image: OdinLogo,
     slug: 'pbs',
     stack: ['JavaScript', 'React', 'React Query'],
+    url: 'https://rev.io/solutions/odin/',
     description:
       'During my 1.5 years at Park Bench Solutions, I served as a front-end developer, specializing in the development of a robust React portal that integrated seamlessly with the powerful Odin API. This experience allowed me to hone my skills in JavaScript and React within a dynamic and high-impact production environment. I was trusted to implement a state management system that could scale and grow along with the front end. The stack for this project is React, React Query, PHP',
-    url: 'https://rev.io/solutions/odin/',
+    challenges:
+      "Many challenges arose during my time at Park Bench Solutions, such as implementing a scalable state management system, as well as API integration and data handling. Integrating the powerful Odin API required careful consideration of data types and handling, React Query's robust state management API was a great choice for this.",
   },
 ];
