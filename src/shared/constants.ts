7e2f4b73c0ce71cbd5c70233538263e0fa46c8a7
Zak Mayfield <zakmayfield@gmail.com>
2023-07-18 11:19:21 -0400

    update project content

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -66,7 +66,7 @@ export const projects: ProjectsType = [
     stack: ['JavaScript', 'React', 'Storybook'],
     url: 'https://www.dominos.com',
     description:
-      "During my time at Dominos, I played a crucial role as a front end React developer, actively contributing to the continuous development and enhancement of the Dominos web app. My primary focus was on ensuring the smooth operation and improvement of user interfaces. Leveraging my skills in React and JavaScript, I actively participated in maintaining and elevating the app's functionality. I collaborated with Dominos UX/UI designers to ensure consistensy across the front end and delivered high quality, trusted code. The stack I utilized here was React, JavaScript, and Storybook",
+      "During my time at Dominos, I played a crucial role as a front end React developer, actively contributing to the continuous development and enhancement of the Dominos web app. My primary focus was on ensuring the smooth operation and improvement of user interfaces. Leveraging my skills in React and JavaScript, I actively participated in maintaining and elevating the app's functionality. I collaborated with Dominos backend developers and UX/UI designers to ensure consistensy across the front end and delivered high quality, trusted code. The stack I utilized here was React, JavaScript, and Storybook",
     challenges:
       'I would overcome several challenges in my postion here at Dominos, such as ensuring accesibility and cross browser compatibility, as well as scalability for a large production environment, and ensuring code was performant and clean. I learned how to build UI components in Storybook to ensure reuseability and scalability for the front end.',
   },
@@ -78,8 +78,8 @@ export const projects: ProjectsType = [
     stack: ['JavaScript', 'React', 'React Query'],
     url: 'https://rev.io/solutions/odin/',
     description:
-      'During my 1.5 years at Park Bench Solutions, I served as a front-end developer, specializing in the development of a robust React portal that integrated seamlessly with the powerful Odin API. This experience allowed me to hone my skills in JavaScript and React within a dynamic and high-impact production environment. I was trusted to implement a state management system that could scale and grow along with the front end. The stack for this project is React, React Query, PHP',
+      'During my 1.5 years at Park Bench Solutions, I served as a front-end developer, specializing in the development of a robust React portal that integrated seamlessly with the powerful Odin API. PORT. This experience allowed me to hone my skills in JavaScript and React within a dynamic and high-impact production environment. I was trusted to implement a state management system that could scale and grow along with the front end as well as porting the legacy AngularJS app to React. The stack for this project is React, React Query, PHP.',
     challenges:
-      "Many challenges arose during my time at Park Bench Solutions, such as implementing a scalable state management system, as well as API integration and data handling. Integrating the powerful Odin API required careful consideration of data types and handling, React Query's robust state management API was a great choice for this.",
+      "Many challenges arose during my time at Park Bench Solutions, such as implementing a scalable state management system, as well as API integration and data handling. Integrating the Odin API required careful consideration of data types and handling, React Query's robust state management API was a great choice for this.",
   },
 ];
